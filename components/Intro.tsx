488c1b254b4e6ad8ef9d078fdedc1c75bd4cfeac
siwakorn-chantawat <siwakorn.chantawat@gmail.com>
2024-04-02 19:49:57 +0700

    update new cv

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -92,7 +92,7 @@ export default function Intro() {
           className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full 
         outline-none focus:scale-110 hover:scale-110 active:scale-105 transition
         cursor-pointer borderBlack dark:bg-white/10 dark:text-white "
-          href="/CV.pdf"
+          href="/siwakorn-chantawat-cv.pdf"
           download
         >
           Download CV
